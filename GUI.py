eaef02387891576ba3ca3707dff56018a565570e
Michał <michal@pop-os>
2020-11-04 21:09:20 +0100

    Update GUI.py and functions.py

--- GUI.py
+++ GUI.py
@@ -6,12 +6,24 @@ root = Tk()
 root.title('password generator v 1.0')
 root.resizable(0, 0)
 
+varDigi = IntVar()
+varChLower = IntVar()
+varChUpper = IntVar()
+
 lblPassword = Label(root, text='....................')
-lblPassword.grid(row=0, column=0, padx=50, pady=50)
-btnGenerate = Button(root, text='Generate', command=lambda: functions.generate_password(1, 1, 1))
-btnGenerate.grid(row=0, column=1)
+lblPassword.grid(row=0, column=1, padx=10, pady=15)
+btnGenerate = Button(root, text='Generate', command=lambda: functions.generate_password())
+btnGenerate.grid(row=1, column=0, padx=10)
 btnCopy = Button(root, text='Copy', command=lambda: functions.copy_pass())
-btnCopy.grid(row=0, column=2, padx=10)
+btnCopy.grid(row=1, column=2, padx=10)
+chDigits = Checkbutton(root, text='Digits', variable=varDigi, onvalue=1, offvalue=0)
+chDigits.grid(row=2, column=0, padx=10, pady=7)
+chChLower = Checkbutton(root, text='Lower-case', variable=varChLower, onvalue=1, offvalue=0)
+chChLower.grid(row=2, column=1, padx=10, pady=7)
+chChUpper = Checkbutton(root, text='Upper-case', variable=varChUpper, onvalue=1, offvalue=0)
+chChUpper.grid(row=2, column=2, padx=10, pady=7)
+lblError = Label(root, text='')
+lblError.grid(row=3, column=1, pady=7)
 
 
 def gui_start():
--- functions.py
+++ functions.py
@@ -4,17 +4,52 @@ import string
 import GUI
 
 
-def generate_password(chsm, chbig, digit):
-    strin = string.ascii_letters
+def generate_password():
     password = ''
-    if chsm == 1 & chbig == 1 & digit == 1:
+    if (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_letters
         for i in range(10):
             chornumb = random.choice(['ch', 'digi'])
             if chornumb == 'ch':
                 password = password + random.choice(strin)
             else:
                 password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 0):
+        strin = string.ascii_lowercase
+        for i in range(10):
+            chornumb = random.choice(['ch', 'digi'])
+            if chornumb == 'ch':
+                password = password + random.choice(strin)
+            else:
+                password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_uppercase
+        for i in range(10):
+            chornumb = random.choice(['ch', 'digi'])
+            if chornumb == 'ch':
+                password = password + random.choice(strin)
+            else:
+                password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_letters
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_uppercase
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 0):
+        strin = string.ascii_lowercase
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 0):
+        for i in range(10):
+            password = password + str(random.randint(0, 10))
+    else:
+        GUI.lblError.config(text='error!')
     # print(password)
+    print(str(GUI.varDigi.get()) + ', ' + str(GUI.varChLower.get()) + ', ' + str(GUI.varChUpper.get()))
+    # print(strin)
     GUI.lblPassword.config(text=password)
 
 
